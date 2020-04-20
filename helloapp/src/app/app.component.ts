c0d6d1b1fb6e9ef6b619afaf8717da7f808be039
alexander.ocheev <alexander.ocheev@travelline.ru>
2020-04-20 22:02:18 +0300

    LESS-5. Привязка данных

--- helloapp/src/app/app.component.ts
+++ helloapp/src/app/app.component.ts
@@ -3,12 +3,43 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <label>Введите имя:</label>
-        <input [(ngModel)]="name" placeholder="name" />
-        <h1>Добро пожаловать {{ name }}!</h1>
+        <h2>Привязка элемента (односторонняя)</h2>
+        <p>Имя: {{ name }}</p>
+        <p>Возраст: {{ age }}</p>
+        <br/><br/>
+
+        <h2>Привязка свойств элементов (односторонняя)</h2>
+        <input type="text" [value]="name">
+        <p [textContent]="name"></p>
+        <br/><br/>
+
+        <h2>Привязка к событию</h2>
+        <p>Количество кликов {{ count }}</p>
+        <button (click)="increase()">Click: (click)="increase()</button>
+        <br/><br/>
+        <button on-click="increase()">Click: on-click="increase()</button>
+        <br/><br/>
+        <button (click)="increaseEvent($event)">Click: (click)="increaseEvent($event)</button>
+        <br/><br/>
+    
+        <h2>Двусторонняя привязка</h2>
+        <p>Привет {{ name }}</p>
+        <input type="text" [(ngModel)]="name" />
+        <br/><br/>
+        <input type="text" [(ngModel)]="name" />
     `
 })
 
 export class AppComponent {
-    name = ''
+    name: string = 'Tom';
+    age: number = 25;
+
+    count: number = 0;
+    increase(): void {
+        this.count++;
+    }
+    increaseEvent($event): void {
+        this.count++;
+        console.log($event);
+    }
 }
